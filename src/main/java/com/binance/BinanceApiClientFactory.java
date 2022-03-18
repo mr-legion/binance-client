2f99715bae920e48afb47a858bedbb2529aec8cc
Oleh Petriv <mr.oleh.petriv@gmail.com>
2022-03-18 17:15:07 +0200

    Add rate limit function

--- src/main/java/com/binance/BinanceApiClientFactory.java
+++ src/main/java/com/binance/BinanceApiClientFactory.java
@@ -14,18 +14,31 @@ public class BinanceApiClientFactory {
 
     private final BinanceApiServiceGenerator serviceGenerator;
 
-    private final ApiCredentials apiCredentials;
+    private ApiCredentials apiCredentials;
 
     public BinanceApiClientFactory() {
-        this(new OkHttpClient(), null);
+        this.serviceGenerator = new BinanceApiServiceGenerator(new OkHttpClient());
     }
 
     public BinanceApiClientFactory(ApiCredentials apiCredentials) {
-        this(new OkHttpClient(), apiCredentials);
+        this.serviceGenerator = new BinanceApiServiceGenerator(new OkHttpClient());
+        this.apiCredentials = apiCredentials;
     }
 
-    private BinanceApiClientFactory(OkHttpClient client, ApiCredentials apiCredentials) {
-        this.serviceGenerator = new BinanceApiServiceGenerator(client);
+    public BinanceApiClientFactory(ApiCredentials apiCredentials, ApiInteractionConfig apiInteractionConfig) {
+        this(new OkHttpClient(), apiCredentials, apiInteractionConfig);
+    }
+
+    private BinanceApiClientFactory(OkHttpClient client,
+                                    ApiCredentials apiCredentials,
+                                    ApiInteractionConfig apiInteractionConfig) {
+        OkHttpClient newClient = client.newBuilder()
+                .proxySelector(new CustomProxySelector(apiInteractionConfig.getProxies()))
+                .addInterceptor(new RateLimitInterceptor(
+                        apiInteractionConfig.getMaxRequestsPerSecond(),
+                        apiInteractionConfig.getMaxApiKeyUsagePerSecond()
+                )).build();
+        this.serviceGenerator = new BinanceApiServiceGenerator(newClient);
         this.apiCredentials = apiCredentials;
     }
 
